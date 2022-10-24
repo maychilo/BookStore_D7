044718233b41a3e49dfb0e24e12274c932131164
May Chi <116498227+maychilo@users.noreply.github.com>
2022-10-25 00:40:54 +1100

    Update BookStoreMain.java

--- BookStoreMain.java
+++ BookStoreMain.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class BookStoreMain {
+public class BookStoreMain extends BookStoreService {
 
 	public static void main(String[] args) {
 		System.out.println(
